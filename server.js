e8a8b0244ff7921a220adc746c53ba4ef68fe390
“parepallirevanth” <parepallirevanth@gmail.com>
2020-05-04 04:00:19 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    bodyparser = require('body-parser'),
+    connectdb = require('mongoose'),
+    swagger_ui = require('swagger-ui-express'),
+    swagger_doc = require('./app/lib/swagger-ui/api_docs')
+
+require('./app/routes/routes')(app);
+app.use(bodyparser.json());
+
+connectdb.connect('mongodb://localhost:27017/test', { useNewUrlParser: true ,useCreateIndex: true, useUnifiedTopology: true });
+const connection = connectdb.connection;
+connection.once ('open',() => {
+    console.log("Mongodb database connection established succesfully");
+})
+
+app.use('/swagger/api', swagger_ui.serve, swagger_ui.setup(swagger_doc));
+
+app.listen(3000, () => {
+    console.log("Server is running on port: " + 3000);
+    connectdb;
+});
